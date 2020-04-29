c2c14164db3a255ef7d1b7c97f0effc561ef0649
Maxim Kopylov <31179849+fureinzz@users.noreply.github.com>
2020-04-29 18:59:57 +0300

    Added `CollapseElement` constructor

--- fureinzz-collapse.js
+++ fureinzz-collapse.js
@@ -1,1 +1,6 @@
 import { html, LitElement } from './node_modules/lit-element/lit-element';
+
+export class CollapseElement extends LitElement {
+    
+}
+customElements.define('fr-collapse', CollapseElement);
