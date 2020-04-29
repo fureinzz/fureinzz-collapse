a92c3ee6772e1701b344aa60aba02faedaffbdaf
Maxim Kopylov <31179849+fureinzz@users.noreply.github.com>
2020-04-29 19:05:29 +0300

    Added `opened` property

--- fureinzz-collapse.js
+++ fureinzz-collapse.js
@@ -1,6 +1,15 @@
 import { html, LitElement } from './node_modules/lit-element/lit-element';
 
 export class CollapseElement extends LitElement {
-    
+    constructor() {
+        super();
+        this.opened = false;
+    }
+    static get properties() {
+        return {
+            opened: { type: Boolean, reflect: true, attribute: true },
+        };
+    }
+
 }
 customElements.define('fr-collapse', CollapseElement);
