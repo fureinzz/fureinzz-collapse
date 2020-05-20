8f824b856acf7ed3097982c78488c4a92240b5c1
Maxim Kopylov <31179849+fureinzz@users.noreply.github.com>
2020-05-20 17:01:25 +0300

    Update test files

--- test/fureinzz-collapse.test.js
+++ test/fureinzz-collapse.test.js
@@ -1,23 +1,46 @@
-import 'wct-browser-legacy/browser'
-
 suite('<fureinzz-collapse>', () => {
     let component
 
     setup(() => component = document.querySelector('fureinzz-collapse'))
 
-    test('`opened` attribute', () => {
+    test('default `opened` attribute', () => {
         assert.equal(component.opened, false)
     })
 
-    test('`noAnimation` attribute', () => {
+    test('default `noAnimation` attribute', () => {
         assert.equal(component.noAnimation, false)
     })
 
-    test('`horizontal` attribute', () => {
+    test('default `horizontal` attribute', () => {
         assert.equal(component.horizontal, false)
     })
 
     test('default opened maxHeight', () => {
         assert.equal(component.style.maxHeight, '0px')
     })
+
+    test('default display value', () => {
+        assert.equal(component.style.display, 'none')
+    })
+
+    test('sdads', done => {
+        component.addEventListener('transitionend', () => {
+            if(component.opened) {
+                assert.equal(component.opened, true)
+                assert.equal(component.style.maxHeight, '')
+                assert.equal(component.style.display, '')
+
+                done()
+            } else {
+                assert.equal(component.opened, false)
+                assert.equal(component.style.maxHeight, '0px')
+                assert.equal(component.style.display, 'none')
+                
+
+                done()
+            }
+        })
+
+        component.opened = true
+    })    
 })
